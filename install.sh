9d0bf41d876ff2d0e03e6be6754320137e6ea4b9
Aron Tinkl <arontinkl@gmail.com>
2025-07-02 22:28:22 +0200

    feat: first installation question

---
+++ install.sh
@@ -0,0 +1,28 @@
+#                                       __             __               
+#                                      |  \           |  \              
+# ______ ____   __    __           ____| $$  ______  _| $$_     _______ 
+# |      \    \ |  \  |  \ ______  /      $$ /      \|   $$ \   /       \
+# | $$$$$$\$$$$\| $$  | $$|      \|  $$$$$$$|  $$$$$$\\$$$$$$  |  $$$$$$$
+# | $$ | $$ | $$| $$  | $$ \$$$$$$| $$  | $$| $$  | $$ | $$ __  \$$    \ 
+# | $$ | $$ | $$| $$__/ $$        | $$__| $$| $$__/ $$ | $$|  \ _\$$$$$$\
+# | $$ | $$ | $$ \$$    $$         \$$    $$ \$$    $$  \$$  $$|       $$
+#  \$$  \$$  \$$ _\$$$$$$$          \$$$$$$$  \$$$$$$    \$$$$  \$$$$$$$ 
+#              |  \__| $$                                               
+#               \$$    $$                                               
+#                \$$$$$$                                                
+#                
+#
+#ASCII generated on:( https://patorjk.com/software/taag/)
+
+# Check out the installation script down below.
+echo " 
+This is a dotfile installation script. Do you want to continue the installation? 
+"
+
+read -rp "Do you want to continue? [y/n] " answer
+case "$answer" in
+  [Yy]) echo "Continuing...";;
+  [Nn]) echo "Exiting."; exit 1;;
+  *) echo "Invalid input."; exit 2;;
+esac
+#There will be a wiki page appearing for this soon. Check out: (https://github.com/Aron22563/my-dots/wiki/Roadmap)
